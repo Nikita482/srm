7ab32bd4d4d8ceeea49610adb81ff1dde50022bb
Nikita <nekitakytilov@gmail.com>
2026-07-07 15:27:07 +0300

    подключел бд

---
+++ server/config/db.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+await mongoose.connect("mongodb://127.0.0.1:27017/srm_bd");
+console.log("MongoDB connected");
+
+const userSchema = new mongoose.Schema({
+  name: String,
+});
+
+const User = mongoose.model("User", userSchema);
+
+await User.create({ name: "AlexXX2" });
+console.log("User created");
