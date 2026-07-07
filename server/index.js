18189575c0729cee6439b936059c815099ff095d
Nikita <nekitakytilov@gmail.com>
2026-07-07 12:46:42 +0300

    база фронта и бэк с экспресом но без бд

---
+++ server/index.js
@@ -0,0 +1,15 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Сервер запущен!");
+});
+
+app.listen(3000, () => {
+  console.log("Server started on 3000");
+});
