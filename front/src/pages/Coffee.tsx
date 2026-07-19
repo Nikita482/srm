d5efb679ae1e55a53e81be72e4ff2fd51f5b3367
Nikita <nekitakytilov@gmail.com>
2026-07-19 16:27:45 +0300

    добавил последний месяц в селект и таблицу по умолчанию

--- front/src/pages/Coffee.tsx
+++ front/src/pages/Coffee.tsx
@@ -112,6 +112,56 @@ const CoffeePage = () => {
         },
       ],
     },
+    {
+      id: 3,
+      month: "Август 2026",
+      data: [
+        {
+          key: 1,
+          date: "5",
+          salary: 2500,
+          expenses: 0,
+          collection: 0,
+          paid: 0,
+          comment: "",
+          accrued: 0,
+          remaining: 2500,
+        },
+        {
+          key: 2,
+          date: "9, 11",
+          salary: 5000,
+          expenses: 150,
+          collection: 3000,
+          paid: 0,
+          comment: "ччч",
+          accrued: 2000,
+          remaining: 3000,
+        },
+        {
+          key: 3,
+          date: "16, 17, 20",
+          salary: 7500,
+          expenses: 0,
+          collection: 2000,
+          paid: 1000,
+          comment: "",
+          accrued: 3000,
+          remaining: 4500,
+        },
+        {
+          key: 4,
+          date: "25, 26, 27",
+          salary: 7500,
+          expenses: 0,
+          collection: 0,
+          paid: 5000,
+          comment: "",
+          accrued: 5000,
+          remaining: 2500,
+        },
+      ],
+    },
   ]);
   const columns = [
     { title: "Число", dataIndex: "date" },
@@ -123,7 +173,9 @@ const CoffeePage = () => {
     { title: "Начислено", dataIndex: "accrued" },
     { title: "Осталось", dataIndex: "remaining" },
   ];
-  const [selectedMonth, setSelectedMonth] = useState(null);
+  const [selectedMonth, setSelectedMonth] = useState(
+    months[months.length - 1].id,
+  );
 
   // создание месяца
   const createMonth = () => {
@@ -152,9 +204,9 @@ const CoffeePage = () => {
       <button onClick={() => createMonth()}>new month</button>
       <Select
         style={{ width: 150 }}
-        defaultValue="месяц"
+        value={selectedMonth}
         options={monthOptions}
-        onChange={(value) => setSelectedMonth(value)}
+        onChange={(value) => setSelectedMonth(Number(value))}
       />
       <Table columns={columns} dataSource={currentMonth?.data} />
     </>
